6c7195538be756b4485e5107bf9fcd9297eb8c96
Pushkar Gupta <guptapushkar86@gmail.com>
2025-05-11 18:13:45 +0530

    add details about GOPATH

--- 01_hello/main.go
+++ 01_hello/main.go
@@ -4,3 +4,13 @@ func main() {
 	// use fp as shortcut just like sysout in Java
 	fmt.Println("Hello, Go!")
 }
+
+
+/*
+	GOPATH defines your Go workspace and was essential for project and dependency management before Go modules. 
+	For modern Go web backend development, prefer Go modules, but understanding GOPATH is useful when dealing with older code or specific environments
+*/
+
+// Environment Variables
+// setting or piece of information-like a password, file path, or configuration-that programs can look up and use while they are running
+// DB_URL to tell your web app where to find its database, or APP_MODE to switch between development and production settings
